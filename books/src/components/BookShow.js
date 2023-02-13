97cf2eb894ffcb28b5270ae1895c0f389c562cc5
CELINE ZIJING LIU <mangolzj@gmail.com>
2023-02-13 09:45:52 -0800

    added context system and refactored components

--- books/src/components/BookShow.js
+++ books/src/components/BookShow.js
@@ -1,15 +1,16 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import BooksContext from "../context/books";
 import BookEdit from "./BookEdit";
 function BookShow({ book, onDelete, onEdit }) {
   const [showEdit, setShowEdit] = useState(false);
+  const { deleteBookById } = useContext(BooksContext);
 
   const handleEditClick = () => {
     setShowEdit(!showEdit);
   };
 
-  const handleSubmit = (id, newTitle) => {
+  const handleSubmit = () => {
     setShowEdit(false);
-    onEdit(id, newTitle);
   };
 
   let content = <h3>{book.title}</h3>;
@@ -18,7 +19,7 @@ function BookShow({ book, onDelete, onEdit }) {
   }
 
   const handleDeleteClick = (event) => {
-    onDelete(book.id);
+    deleteBookById(book.id);
     setShowEdit(event.target.value);
   };
 
--- books/src/index.js
+++ books/src/index.js
@@ -1,3 +1,4 @@
+import { Provider } from "./context/books";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -5,4 +6,9 @@ import "./index.css";
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+//element inside the provider can access the value stored in the context
+root.render(
+  <Provider>
+    <App />
+  </Provider>
+);
