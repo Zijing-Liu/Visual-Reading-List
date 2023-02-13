3e63521837eff8717dc35ab784ced997e7cda82d
CELINE ZIJING LIU <mangolzj@gmail.com>
2023-02-13 09:58:20 -0800

    updated the hooks with customized hook

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,9 +1,14 @@
-import { useContext } from "react";
-import BooksContext from "../context/books";
+// import { useContext } from "react";
+// import BooksContext from "../context/books";
 import BookShow from "./BookShow";
+import useBooksContext from "../hooks/use-books-context";
 
+// function useBooksContext() {
+//   return useContext(BooksContext);
+// }
+// use customized hooks
 function BookList() {
-  const { books } = useContext(BooksContext);
+  const { books } = useBooksContext();
 
   const renderedBooks = books.map((book) => {
     return <BookShow key={book.id} book={book} />;
